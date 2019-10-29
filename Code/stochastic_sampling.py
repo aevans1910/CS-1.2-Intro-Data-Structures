dcf90a5981fdc218917ce0f8379e32c9b6602dde
Ariane Evans <ariane.evans@student.makeschool.com>
2019-10-29 15:09:16 -0700

    [add] test function

--- Code/stochastic_sampling.py
+++ Code/stochastic_sampling.py
@@ -1,14 +1,19 @@
-def histogram():
-    ''' A function that return a histogram data structure that stores each unique 
-    word along with the number of times the word appears in the source text'''
+import random
 
-    file = 'Grim-tales.txt'
+def get_text():
+    file = 'random.txt'
 
     with open(file, 'r') as f:
         text = f.read().split()
+    
+    return text
+
+def histogram(get_text):
+    ''' A function that return a histogram data structure that stores each unique 
+    word along with the number of times the word appears in the source text'''
 
     dictionary = {}
-    for word in text:
+    for word in get_text:
 
         if word in dictionary.keys():
             dictionary[word] += 1
@@ -16,3 +21,26 @@ def histogram():
             dictionary[word] = 1
 
     return dictionary
+
+def sample_frequency(histogram):
+    '''A function that takes a histogram and returns a single word, at random'''
+    
+    words_list = []
+
+    for key in histogram:
+        for _ in range(histogram[key]):
+            words_list.append(key)
+
+    random_word = random.choice(words_list)
+    return (random_word)
+
+if __name__ == '__main__':
+    histo = histogram(get_text())
+
+    results = []
+
+    for counter in range(100):
+        random_word = sample_frequency(histo)
+        results.append(random_word)
+
+    print (results)
