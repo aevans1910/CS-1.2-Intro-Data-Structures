2f2f5610a2c01ef509a997a10e815b8af800b514
Ariane Evans <ariane.evans@student.makeschool.com>
2019-11-07 22:46:08 -0800

    finish clean up of files

--- Code/My own files/app.py
+++ Code/My own files/app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def sampling():
+    '''This function calls the histogram function on a sample text. This will then pick a 
+    random word and return it'''
     histo = sample.histogram(sample.get_text())
     random_words = sample.sample_frequency(histo)
     
