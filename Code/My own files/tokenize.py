d3820f229d3b5f9f26dac276260743ed2e6b0d89
Ariane Evans <ariane.evans@student.makeschool.com>
2019-11-07 22:25:03 -0800

    clean up tokenize.py

--- Code/My own files/tokenize.py
+++ Code/My own files/tokenize.py
@@ -1,15 +1,18 @@
 import random
 
 #Fisher-Yates function that generates a random order for the words in script
-def random_script_order(script, n):
-    for i in range(n):
-        j = random.randint(0, i)
+def random_script_order(script, length):
+    #This code randomly selects one word from the script, then randomly selects another
+    #word from the script, and then switches the two. It does this for every word in
+    #the script
+    for selected_word1 in range(length):
+        selected_word2 = random.randint(0, selected_word1)
 
-        script[i],script[j] = script[j],script[i]
+        script[selected_word1],script[selected_word2] = script[selected_word2],script[selected_word1]
     return script
 
 script = ['hello', 'earthlings', 'I', 'have', 'come', 'to', 'dominate', 'your', 'planet']
-n = len(script)
-print(random_script_order(script,n))
+length = len(script)
+print(random_script_order(script,length))
     
         
