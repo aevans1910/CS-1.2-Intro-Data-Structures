a2a4ba487e32768afe940065b6bb4a81e9d37f9e
Ariane Evans <ariane.evans@student.makeschool.com>
2019-11-17 21:23:12 -0800

    [add] first attempt at full markov chain code

--- markov_chain.py
+++ markov_chain.py
@@ -3,15 +3,15 @@ import random
 
 class MarkovChain(dict):
 
-    def __init__(self, word_list=None):
+    def __init__(self, words_list=None):
         '''Initilising the Markiv Chain'''
         super(MarkovChain, self).__init__()
 
-        if word_list is not None:
-            self.create_markov_chain(word_list)
+        if words_list is not None:
+            self.create_markov_chain(words_list)
 
 
-    def create_markov_chain(self, word_list):
+    def create_markov_chain(self, words_list):
         '''Creating the markov chain'''
         for index, word in enumerate(words_list):
 
@@ -35,4 +35,34 @@ class MarkovChain(dict):
         else:
             return None
 
-    
+    def random_sentence(self):
+        random_word = random.choice(list(self.keys))
+        first_word = random_word.capitalize()
+        words = [first_word]
+        other_random_words = random.choice(list(self.keys))
+
+        for _ in range(len - 1):
+            
+            other_random_words = self.sample(other_random_words)[0]
+            words.append(other_random_words)
+
+        return ' '.join(words) + '.'
+
+def clean_up_words(file_name):
+    '''Cleans up words so we can use them anywhere
+    in the sentence'''
+    with open(file_name, 'r') as f:
+        words = f.read().split()
+
+    word_list = []
+    for word in words:
+        word = word.strip(".@'/").lower()
+        word_list.append(word)
+
+    return word_list
+
+if __name__ == "__main__":
+    words_list = clean_up_words('random_sentence.txt')
+    markov_sentence = MarkovChain(words_list=words_list)
+
+    print(markov_sentence.random_sentence())
