6ba4ddcabe327d27ce0e3c116388eca4ad18bbfd
Ariane Evans <ariane.evans@student.makeschool.com>
2019-12-11 16:14:57 -0800

    [finish fix] markov chain

--- my_code/markov_chain.py
+++ my_code/markov_chain.py
@@ -17,21 +17,16 @@ class MarkovChain(dict):
         self[word_1].append(word_2)
 
     def random_sentence(self, length=8):
-        # Pick a random word from the original histogram
-        word = self.sample()
-        # Create a sentence list with the first sampled word as its start
-        sentence = [word]
-        # Create a new markov chain from the histogram
-        markov_histogram = self.create_markov_chain()
-        # Until we reach the length of 8
-        for _ in range(length-1):
-            # The next word will be randomly sampled from the first words histogram
-            next_word = markov_histogram[word].sample()
-            # Make word the next word
-            word = next_word
-            # Add the next word to the sentence
+        sentence = []
+        
+        while len(sentence) < 10:
+            start_word = random.choice(list(self.keys()))
+            next_word = self[start_word].sample()
+
+            sentence.append(start_word)
             sentence.append(next_word)
-        # Join the words together to return something like a string
+
+            start_word = next_word
         return ' '.join(sentence)
 
 
@@ -40,10 +35,7 @@ if __name__ == "__main__":
     # words_list = ["a", "man", "a", "plan", "a", "canal"]
     markov_sentence = MarkovChain(words_list)
     # print(markov_sentence)
-    for keys in markov_sentence.keys():
-        print(keys, markov_sentence[keys])
+    # for keys in markov_sentence.keys():
+    #     print(keys, markov_sentence[keys])
 
-    # print(markov_sentence.random_sentence())
-    # nested_histo = markov_sentence.create_markov_chain(words_list)
-
-    # print(nested_histo['fish'].sample())
+    print(markov_sentence.random_sentence())
